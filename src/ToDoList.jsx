aa338f7dca7f651c626f9ef6b261e3917d2d609b
IOSA33 <sedinkin33@gmail.com>
2026-05-28 01:49:55 +0300

    version 1.35, added lists, not working yet version

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,61 +1,220 @@
-import { useState, useEffect} from 'react';
-import React from 'react';
-import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor , useSensors } from '@dnd-kit/core';
-import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable';
-import SortableItem from './SortableItem';
+import { useState, useEffect} from 'react'
+import React from 'react'
+import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCenter, useSensor , useSensors } from '@dnd-kit/core'
+import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable'
+import SortableItem from './SortableItem'
+
+const Title = () => {
+  return (
+   <h1 className="mx-auto m-3 mt-6 mb-6 text-2xl"> To Do List  v1.3</h1>
+  )
+}
+
+const ShowMenu = ({ newTask, handleInputChange, addTask, bucket, addList, setShowLists }) => {
+  return (
+    <div className="flex space-x-4 mb-4 flex-wrap">
+        <input
+            className="max-w-xl mb-1 w-full h-10 border-2 border-solid border-gray-300 rounded-lg p-2"
+            type="text"
+            placeholder="Enter a Todo"
+            value={newTask}
+            onChange={handleInputChange} 
+          />
+
+        <div className='flex items-center gap-4'> 
+            <button
+              className="px-9 h-10 mb-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+              onClick={addTask} > Add 
+            </button>
+
+          <button 
+            className='px-5 h-10 mb-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition'
+            onClick={bucket}
+          > 
+            🗑️
+          </button>
+
+          <button className='px-5 h-10 mb-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition' 
+            onClick={addList}>
+            Add List
+          </button>
+
+          <button className='px-5 h-10 mb-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition' 
+            onClick={setShowLists}>
+            Show Lists?
+          </button>
+        </div>
+
+      </div>
+  )
+}
+
+const ShowLists = ({ lists, currentListOnClick, setCurrentListID, showLists }) => {
+  if (showLists === false) {
+    return (
+      <></>
+    )
+  } else {  
+    return (
+      <div>
+        Current Lists
+        <br></br>
+        {lists.map(list => {
+          return (
+              <button key={list.id} className='px-5 h-10 mb-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition'
+                  type="button" onClick={() => { 
+                    currentListOnClick(list.list)
+                    setCurrentListID(list.id)
+                  }}>
+
+                  {list.name}
+              </button>
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+const ShowToDoes = ({ list, sensors, closestCenter, handleDragEnd, verticalListSortingStrategy, done, deleteTask }) => {
+  if (list?.length === 0) {
+    return (
+      <></>
+    )
+  } else {
+   return (
+      <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+        <SortableContext items={list} strategy={verticalListSortingStrategy}>
+
+          <ul className=''>
+              {list.map((task) => (
+                <SortableItem  key={task.id} id={task.id}>
+
+                  <li className="mb-2 bg-white p-3 rounded-lg shadow-md hover:bg-gray-100 transition duration-300 flex justify-between items-start">
+                    <div className="flex items-center flex-grow">
+                      <input 
+                        type='checkbox' 
+                        onChange={() => done(task.id)}
+                        checked={task.done}
+                        className="appearance-none w-5 h-5 border-2 border-gray-300 rounded-md checked:bg-green-500 checked:border-green-500
+                                relative cursor-pointer transition-colors flex-shrink-0"
+                      />
+                      <span className="ml-2 text-lg font-semibold text-gray-700 break-all flex-grow">
+                        {task.text}
+                      </span>
+                    </div>
+
+                    <button
+                      onClick={() => deleteTask(task.id)} 
+                      className="ml-3 hover:text-red-600 transition duration-200" >
+                      🔥
+                    </button>
+                  </li>
+
+                </SortableItem> 
+              ))}
+          </ul>
+
+        </SortableContext>
+      </DndContext>
+    )
+  }
+}
 
 
-function TodoList() {
-
-    const [tasks, setTasks] = useState(() => {
-        const savedTodos = localStorage.getItem('Todos');
-        return savedTodos ? JSON.parse(savedTodos) : [];
-    });
-
-    const [newTask, setNewTask] = useState("");
-    const[deleted, setNewDelete] = useState([]);
+const TodoList = () => {
+    const [tasks, setTasks] = useState()
+    const [newTask, setNewTask] = useState("")
+    const [deleted, setNewDelete] = useState([])
+    const [search, setSearch] = useState("")
+    const [list, setList] = useState([])
+    const [lists, setLists] = useState(() => {
+        const savedTodos = localStorage.getItem('Todos')
+        return savedTodos ? JSON.parse(savedTodos) : []
+    })
+    const [showLists, setShowLists] = useState(() => {
+        const savedshowLists = localStorage.getItem('ShowList')
+        return savedshowLists ? JSON.parse(savedshowLists) : true
+    })
+    const [currentListID, setCurrentListID] = useState(() => {
+        const savedcurrentListID = localStorage.getItem('CurrentListID')
+        return savedcurrentListID ? JSON.parse(savedcurrentListID) : null
+    })
 
     useEffect(() => {
-        localStorage.setItem('Todos', JSON.stringify(tasks));
-    }, [tasks]);
+      if (currentListID && lists.length > 0) {
+          const foundListObject = lists.find(item => item.id === currentListID)
+          
+          if (foundListObject) {
+              setList(foundListObject.list)
+          }
+      }
+    }, [currentListID, lists])
 
-    function handleInputChange(event) {
-        setNewTask(event.target.value);
-    } 
+    useEffect(() => {
+        localStorage.setItem('Todos', JSON.stringify(lists))
+        localStorage.setItem('CurrentListID', JSON.stringify(currentListID))
+        localStorage.setItem('ShowList', showLists)
+    }, [lists, currentListID, showLists])
 
-    function addTask() {
+    const handleInputChange = (event) => {
+        setNewTask(event.target.value)
+    }
+
+    const addList = () => {
+      if (newTask.trim() !== "") {
+        const newListName = { id: crypto.randomUUID(), name: newTask, list: []}
+        const updatedList = [...lists, newListName]
+        setLists(updatedList)
+        setNewTask("")
+      }
+    }
+
+    const addTask = () => {
         if (newTask.trim() !== "") {
-            setTasks([...tasks, { id: crypto.randomUUID(), text: newTask, done: false }]);
-            setNewTask("");
+            const newTodoItem = { id: crypto.randomUUID(), text: newTask, done: false }
+            const updatedList = [...list, newTodoItem]
+
+            setList([...list, newTodoItem])
+            
+            setLists(lists.map(n => {
+              if (n.id === currentListID) {
+                return {...n, list: updatedList}
+              } else {
+                return n
+              }
+            }))
+
+            setNewTask("")
         }
     }
     
-    function deleteTask(id) {
-      const taskToDelete = tasks.find(task => task.id === id);
-      const updatedTasks = tasks.filter(task => task.id !== id);
+    const deleteTask = (id) => {
+      const taskToDelete = tasks.find(task => task.id === id)
+      const updatedTasks = tasks.filter(task => task.id !== id)
       
       if (taskToDelete) {
-          setTasks(updatedTasks);
-          setNewDelete(prev => [...prev, taskToDelete]);
+          setTasks(updatedTasks)
+          setNewDelete(prev => [...prev, taskToDelete])
       }
     }
 
-    function done(id) {
+    const done = (id) => {
       const updatedList = tasks.map(task =>
         task.id === id ? { ...task, done: !task.done } : task
-      );
-      setTasks(updatedList);
+      )
+      setTasks(updatedList)
     }
 
-    function bucket(id) {
+    const bucket = (id) => {
       if (deleted.length > 4) {
-        setNewDelete(prev => prev.slice(1));
+        setNewDelete(prev => prev.slice(1))
       }
 
       if (deleted.length > 0) {
-        const taskToRestore = deleted[deleted.length - 1];
-        setTasks(prev => [...prev, taskToRestore]);
-        setNewDelete(prev => prev.slice(0, prev.length - 1));
+        const taskToRestore = deleted[deleted.length - 1]
+        setTasks(prev => [...prev, taskToRestore])
+        setNewDelete(prev => prev.slice(0, prev.length - 1))
       }
     }
 
@@ -65,98 +224,37 @@ function TodoList() {
       useSensor(KeyboardSensor, {
         coordinateGetter: sortableKeyboardCoordinates,
       })
-    );
+    )
 
-    function handleDragEnd(event) {
-      const { active, over } = event;
+    const handleDragEnd = (event) => {
+      const { active, over } = event
 
       if (active.id !== over.id) {
           setTasks((tasks) => {
-              const oldIndex = tasks.findIndex(task => task.id === active.id);
-              const newIndex = tasks.findIndex(task => task.id === over.id);
-              return arrayMove(tasks, oldIndex, newIndex);
-          });
+              const oldIndex = tasks.findIndex(task => task.id === active.id)
+              const newIndex = tasks.findIndex(task => task.id === over.id)
+              return arrayMove(tasks, oldIndex, newIndex)
+          })
       }
     }
 
-
     return (
-        <div  className="mx-auto content-center">
-            <div className="mx-auto max-w-6xl p-1">
-              <h1 className="mx-auto m-3 mt-6 mb-6 text-2xl"> To Do List  v1.3</h1>
+      <div  className="mx-auto content-center">
+        <div className="mx-auto max-w-6xl p-1">
+          
+          <Title />
 
-              <div className="mx-auto p-3 bg-gray-50 rounded-lg shadow-lg">
+          <div className="mx-auto p-3 bg-gray-50 rounded-lg shadow-lg">
 
-                <div className="flex space-x-4 mb-4 flex-wrap">
-                    <input
-                        className="max-w-xl mb-1 w-full h-10 border-2 border-solid border-gray-300 rounded-lg p-2"
-                        type="text"
-                        placeholder="Enter a Todo"
-                        value={newTask}
-                        onChange={handleInputChange} 
-                      />
+            <ShowMenu newTask={newTask} handleInputChange={handleInputChange} addTask={addTask} bucket={bucket} addList={addList} setShowLists={() => setShowLists(!showLists)}/>
+            <ShowLists lists={lists} currentListOnClick={list => setList(list)} setCurrentListID={id => setCurrentListID(id)} showLists={showLists}/>
+            <ShowToDoes list={list} sensors={sensors} closestCenter={closestCenter} handleDragEnd={handleDragEnd} verticalListSortingStrategy={verticalListSortingStrategy} done={done} deleteTask={deleteTask}/>
 
-                    <div className='flex items-center gap-4'> 
-                        <button
-                          className="px-9 h-10 mb-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-                          onClick={addTask} > Add 
-                        </button>
-
-                      <button 
-                        className='px-5 h-10 mb-1 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition'
-                        onClick={bucket}
-                      > 
-                        🗑️
-                      </button>
-                    </div>
-
-                  </div>
-
-                
-
-              <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-                <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
-
-                  <ul className=''>
-                    {tasks.map((task) => (
-                      <SortableItem  key={task.id} id={task.id}>
-
-                        <li className="mb-2 bg-white p-3 rounded-lg shadow-md hover:bg-gray-100 transition duration-300 flex justify-between items-start">
-                          <div className="flex items-center flex-grow">
-                            <input 
-                              type='checkbox' 
-                              onChange={() => done(task.id)}
-                              checked={task.done}
-                              className="appearance-none w-5 h-5 border-2 border-gray-300 rounded-md checked:bg-green-500 checked:border-green-500
-                                      relative cursor-pointer transition-colors flex-shrink-0"
-                            />
-                            <span className="ml-2 text-lg font-semibold text-gray-700 break-all flex-grow">
-                              {task.text}
-                            </span>
-                          </div>
-
-                          <button
-                            onClick={() => deleteTask(task.id)} 
-                            className="ml-3 hover:text-red-600 transition duration-200" >
-                            🔥
-                          </button>
-                        </li>
-
-                      </SortableItem> 
-                    ))}
-                  </ul>
-
-                </SortableContext>
-              </DndContext>
-
-              </div>
-            </div>
+          </div>
+          
         </div>
-      );
-      
-    
-      
-
+      </div>
+    )
 }
 
-export default TodoList; 
+export default TodoList
